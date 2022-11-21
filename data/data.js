bcc3d72887cd5cafce05a7e0775f069f23aa593e
Amppu <t0haan01@students.oamk.fi>
2022-11-21 17:10:46 +0200

    Improved design and functionality, transferred data to separate file

--- data/data.js
+++ data/data.js
@@ -1,1 +1,68 @@
 // Data
+const dataPublicTransport = [
+    {
+      id: 1,
+      city: "Helsinki",
+      url: "https://www.hsl.fi/"
+    },
+    {
+      id: 2,
+      city: "Imatra",
+      url: "https://www.imatra.fi/asuminen-ja-ymparisto/joukkoliikenne/"
+    },
+    {
+      id: 3,
+      city: "Joensuu",
+      url: "https://jojo.joensuu.fi/"
+    },
+    {
+      id: 4,
+      city: "Kajaani",
+      url: "https://www.kajaaninjoukkoliikenne.fi/"
+    },
+    {
+      id: 5,
+      city: "Kokkola",
+      url: "https://pohjolanmatka.fi/linjaliikenne/kokkolan-paikallisliikenne/"
+    },
+    {
+      id: 6,
+      city: "Kuopio",
+      url: "https://vilkku.kuopio.fi/"
+    },
+    {
+      id: 7,
+      city: "Lappeenranta",
+      url: "https://www.lappeenranta.fi/fi/Kartat-ja-liikenne/Paikallisliikenne/Aikataulut/"
+    },
+    {
+      id: 8,
+      city: "Oulu",
+      url: "https://www.oulunjoukkoliikenne.fi/reitit-ja-aikataulut/"
+    },
+    {
+      id: 9,
+      city: "Pori",
+      url: "https://www.pori.fi/joukkoliikenne"
+    },
+    {
+      id: 10,
+      city: "Rovaniemi",
+      url: "https://www.linkkari.fi/"
+    },
+    {
+      id: 11,
+      city: "Seinäjoki",
+      url: "https://harmanliikenne.fi/seinajoen-paikallisliikenne/"
+    },
+    {
+      id: 12,
+      city: "Tampere",
+      url: "https://www.nysse.fi/"
+    },
+    {
+      id: 13,
+      city: "Vaasa",
+      url: "https://www.vaasa.fi/asu-ja-ela/liikenne-ja-kadut/joukkoliikenne/bussiaikataulut-ja-reitit/"
+    }
+  ]
